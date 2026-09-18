a3ddaf280f6e1a9b26c25f09c6c7ac9890111996
minhinc <tominhinc@gmail.com>
2026-09-18 09:00:57 +0100

    import sys

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-import re,os,subprocess,importlib,shlex,socket
+import re,os,subprocess,importlib,shlex,socket,sys
 
 app = Flask(__name__)
 
